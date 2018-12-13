103656e42e7284a9eaa56db2f7ba84b886f85951
rajcoder7 <rajcoder7@gmail.com>
2018-12-13 09:22:22 +0530

    removed minimum stability from composer.json, added back button partial and included it in all views

--- publish/views/partials/add_btn.blade.php
+++ publish/views/partials/add_btn.blade.php
@@ -1,5 +1,5 @@
 
 {{-- Add new button --}}
-<a href="{{ url('/'.$page.'/create') }}" class="btn btn-success btn-sm add_new_button" title="Add New">
+<a href="{{ url('/'.$page.'/create') }}" class="btn btn-success btn-sm mb-3" title="Add New">
 	<i class="fa fa-plus" aria-hidden="true"></i> Add New
 </a>
--- publish/views/partials/back_btn.blade.php
+++ publish/views/partials/back_btn.blade.php
@@ -1,5 +1,7 @@
 
 {{-- Back button --}}
-{{-- <a href="{{ url('/'.$page) }}" title="Back">
-	<span class="oi oi-chevron-left"></span>
-</a> --}}
+<a href="{{ url($page) }}">
+    <button type="button" class="btn btn-default">
+        Back
+    </button>
+</a>
